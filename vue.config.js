d486b579f6d94239345129f2c3098fac0880e937
fucloud-win11 <270928474@qq.com>
2023-12-02 23:48:58 +0800

    feat: set proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+    transpileDependencies: true,
+    devServer: {
+        https: true,
+        port: 6060,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:6060/',
+                changeOrigin: true,
+                ws: false,
+                secure: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+})
