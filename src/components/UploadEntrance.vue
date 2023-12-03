180ffe03b9be814eeab67a12aaa0e85a39330619
fucloud-win11 <270928474@qq.com>
2023-12-03 14:17:30 +0800

    feat: impl loading and resume

--- src/components/UploadEntrance.vue
+++ src/components/UploadEntrance.vue
@@ -2,7 +2,7 @@
     <el-container style="height: 100%">
         <el-header>
             <el-menu mode="horizontal" background-color="#005E27">
-                <el-menu-item :ellipsis="false"> <img style="width: 140px" :src="logoImage" /> </el-menu-item>
+                <el-menu-item :ellipsis="false"> <img style="width: 140px" :src="logoUrl" /> </el-menu-item>
             </el-menu>
         </el-header>
 
@@ -18,20 +18,28 @@
                     <el-button class="button" style="margin: 20px" color="green">系统介绍</el-button>
                 </div>
 
-                <div class="photo-page" v-show="this.isPhotoing">
+                <div class="photo-page" v-show="this.isPhotoing" v-loading="isRecognizing">
+                    <!-- camera video -->
                     <div class="border" v-if="!hasPhotoed">
                         <video ref="video" id="camera"></video>
-                        <canvas ref="canvas" style="display: none" :width="this.videoWidth" :height="this.videoHeight" id="canvasCamera"></canvas>
+                        <canvas ref="canvas" style="display: none" :width="this.videoWidth" :height="this.videoHeight"></canvas>
                     </div>
+
+                    <!-- recognition result -->
                     <div class="border" v-if="hasPhotoed">
-                        <el-image id="recognition-result" :fit="contain" :src="this.recognizedPhotoUrl"></el-image>
+                        <el-image id="recognition-result" :src="this.recognizedPhotoUrl"></el-image>
                     </div>
+
+                    <!-- operation -->
                     <div class="operation-container">
                         <el-button class="back-button" v-on:click="handleBack" circle>
                             <el-icon><ArrowLeft /></el-icon>
                         </el-button>
-                        <el-button class="photo-button" v-on:click="handlePhoto" color="green" circle>
-                            <el-icon><Select /> </el-icon>
+                        <el-button class="photo-button" v-on:click="handlePhoto" v-if="!hasRecognized" color="green" circle>
+                            <el-icon><CameraFilled /> </el-icon>
+                        </el-button>
+                        <el-button class="resume-button" v-on:click="handleResume" v-if="hasRecognized" color="green" circle>
+                            <el-icon><RefreshRight /></el-icon>
                         </el-button>
                     </div>
                 </div>
@@ -47,24 +55,28 @@
 // eslint-disable-next-line no-unused-vars
 import { recognize, check } from '@/api.js'
 // eslint-disable-next-line no-unused-vars
-import { Select, ArrowLeft } from '@element-plus/icons-vue'
+import { CameraFilled, ArrowLeft, RefreshRight } from '@element-plus/icons-vue'
 
 export default {
     data() {
         return {
-            pictureUrl: '',
-            logoImage: require('@/assets/logo.png'),
-            // isPhotoing: true,
+            logoUrl: require('@/assets/logo.png'),
+            // status
             isPhotoing: false,
             hasPhotoed: false,
+            isRecognizing: false,
+            hasRecognized: false,
+            // video
             recognizedPhotoUrl: null,
             videoWidth: 300,
-            videoHeight: 400
+            videoHeight: 400,
+            constraints: null
         }
     },
     components: {
-        Select,
-        ArrowLeft
+        CameraFilled,
+        ArrowLeft,
+        RefreshRight
     },
     methods: {
         // https://blog.csdn.net/m0_72196169/article/details/134451422
@@ -74,18 +86,26 @@ export default {
             const canvas = this.$refs.canvas
             const context = canvas.getContext('2d')
 
+            // 1. catch image from camera
             context.drawImage(video, 0, 0, canvas.width, canvas.height)
             const dataUrl = canvas.toDataURL('image/jpeg')
+            this.recognizedPhotoUrl = dataUrl
+            this.hasPhotoed = true
 
+            // 2. start recognizing
+            this.isRecognizing = true
             let result = await recognize(dataUrl)
             if (!result.flag) {
                 this.isPhotoing = false
+                console.log(result.data)
                 alert(result.data)
                 return
             }
 
+            // 3. recognize successfully
             this.recognizedPhotoUrl = result.data
-            this.hasPhotoed = true
+            this.isRecognizing = false
+            this.hasRecognized = true
         },
         async handleTest() {
             let result = await check()
@@ -97,9 +117,21 @@ export default {
             this.recognizedPhotoUrl = blobUrl
             this.hasPhotoed = true
         },
+        handleResume() {
+            this.recognizedPhotoUrl = null
+
+            this.hasPhotoed = false
+            this.hasRecognized = false
+
+            this.$nextTick(() => {
+                var video = this.$refs.video
+                this.setCamera(video)
+            })
+        },
         handleBack() {
             this.isPhotoing = false
             this.hasPhotoed = false
+            this.hasRecognized = false
         },
         getCompetence() {
             var video = this.$refs.video
@@ -109,19 +141,37 @@ export default {
                 return
             }
 
+            this.constraints = this.getConstraints()
+            this.setCamera(video)
+
+            this.isPhotoing = true
+        },
+        getConstraints() {
             var constraints = {
-                audio: false,
-                video: {
+                audio: false
+            }
+
+            const userAgent = navigator.userAgent.toLowerCase()
+
+            if (userAgent.match('iphone')) {
+                constraints.video = {
                     facingMode: { exact: 'environment' },
-                    // reverse width and height will be ok
                     width: this.videoHeight,
-                    height: this.videoWidth,
-                    transform: 'scaleX(-1)'
+                    height: this.videoWidth
+                }
+            } else if (userAgent.match('windows')) {
+                constraints.video = {
+                    width: this.videoWidth,
+                    height: this.videoHeight
                 }
             }
 
+            return constraints
+        },
+        // TODO: let it async
+        setCamera(video) {
             navigator.mediaDevices
-                .getUserMedia(constraints)
+                .getUserMedia(this.constraints)
                 .then(function (stream) {
                     if ('srcObject' in video) {
                         video.srcObject = stream
@@ -134,10 +184,14 @@ export default {
                     }
                 })
                 .catch((err) => {
-                    console.log(err)
-                })
+                    if (this.constraints.facingMode != undefined) {
+                        this.constraints.facingMode = undefined
+                        this.setCamera(video, this.constraints)
+                        return
+                    }
 
-            this.isPhotoing = true
+                    alert(err)
+                })
         }
     }
 }
@@ -154,6 +208,8 @@ export default {
 }
 .button {
     flex: 1;
+    width: 400px;
+    max-width: 400px;
     height: 200px;
     margin: 0 20px;
     padding: 5px;
@@ -176,6 +232,7 @@ export default {
 }
 .home-page {
     display: flex;
+    align-items: center;
     justify-content: flex-start;
     flex-direction: column;
     width: 100%;
@@ -193,8 +250,6 @@ export default {
     flex-direction: column;
 
     width: 100%;
-    max-height: 70%;
-    height: 900px;
 }
 #camera {
     flex: 1;
